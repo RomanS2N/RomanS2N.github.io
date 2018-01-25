6d420ada456f667951bd4bd2ce278bc08ac2d839
AnrikoTacatti <anriko.tacatti@gmail.com>
2018-01-25 18:30:01 +0300

    menu

--- rs2/source/templates/default/js/my_ef.js
+++ rs2/source/templates/default/js/my_ef.js
@@ -1,3 +1,4 @@
+
 /* =========================================================
 Accordion
 ========================================================= */
@@ -94,6 +95,20 @@ function myFunction() {
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn2')) {
+    var myDropdown = document.getElementById("myDropdown2");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
       }
