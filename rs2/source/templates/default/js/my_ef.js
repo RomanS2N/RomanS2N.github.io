d2cf4cbcdfbf9be0cadde730f050c92f8b959fc5
AnrikoTacatti <anriko.tacatti@gmail.com>
2018-01-27 20:33:20 +0300

    меню

--- rs2/source/templates/default/js/my_ef.js
+++ rs2/source/templates/default/js/my_ef.js
@@ -84,36 +84,18 @@ Tabs
 
 
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    $("#myDropdown").toggleClass("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
+
 
 function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
+    $("#myDropdown2").toggleClass("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn2')) {
-    var myDropdown = document.getElementById("myDropdown2");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
+
 
 /*===========================================================
 Comments
