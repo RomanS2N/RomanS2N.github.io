9a6088c9600cca1441c710fac9ba805151da5fc7
AnrikoTacatti <anriko.tacatti@gmail.com>
2018-02-13 01:43:26 +0300

    правки

--- rs2/source/templates/default/js/my_ef.js
+++ rs2/source/templates/default/js/my_ef.js
@@ -87,15 +87,30 @@ Tabs
 
 
 function myFunction() {
-    $("#myDropdown").toggleClass("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
 
 
 function myFunction2() {
-    $("#myDropdown2").toggleClass("show");
+    document.getElementById("myDropdown2").classList.toggle("show");
 }
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+     if (!e.target.matches('.dropbtn')) {
+    var myDropdown1 = document.getElementById("myDropdown");
+      if (myDropdown1.classList.contains('show')) {
+        myDropdown1.classList.remove('show');
+      }
+  }
+  if (!e.target.matches('.dropbtn2')) {
+    var myDropdown2 = document.getElementById("myDropdown2");
+      if (myDropdown2.classList.contains('show')) {
+        myDropdown2.classList.remove('show');
+      }
+  }
+}
 
 function vkopenn() {
     $(".vkbox").toggleClass("show");
